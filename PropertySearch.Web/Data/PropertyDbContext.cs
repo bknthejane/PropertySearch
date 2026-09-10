f42317dead2f274b9dbec693678fd10d2406bfcf
Bonolo-Nth <Bonolo.Nthejane@boxfusion.io>
2026-09-10 21:59:15 +0200

    chore: scaffold MVC project with EF Core

---
+++ PropertySearch.Web/Data/PropertyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PropertySearch.Web.Data
+{
+    public class PropertyDbContext : DbContext
+    {
+        public PropertyDbContext(DbContextOptions<PropertyDbContext> options) : base (options)
+        {
+
+        }
+    }
+}
---
+++ PropertySearch.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PropertySearch.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
